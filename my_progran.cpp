79317324842fe45754bb1f5c9a7aedb8b7ed1878
Jorge Ramirez <jorge.ramirez40@hotmail.com>
2021-09-02 20:02:31 -0500

    01. mi primer commit en git para el programa

---
+++ my_progran.cpp
@@ -0,0 +1,16 @@
+<stdio.h>
+<nath.h>
+<getch.h>
+#workspace 'curso devops'
+int main [object ...]{
+int ivalor =0;
+int contador =0;
+cin<< 'ingrese un valor para calcular una suma'
+for [i = 1; i <= ivalor; i++]{
+  icontador+=i;
+}
+count >> 'el resultado es: '+%int,icontador;
+getch();
+exit 1;
+
+}
