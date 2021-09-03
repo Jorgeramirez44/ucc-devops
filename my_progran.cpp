eefaaa9b2f764ec8790351982d84ba7fe1482496
Jorge Ramirez <jorge.ramirez40@hotmail.com>
2021-09-02 20:34:43 -0500

    03. depuracion realizada con el compilador g++ ,se elimina libreria getch.h

--- my_progran.cpp
+++ my_progran.cpp
@@ -1,18 +1,19 @@
 
+#include <iostream>
 #include <stdio.h>
 #include <nath.h>
 #include <getch.h>
 using namespace std;
 
-int main [object ...]{
-int ivalor =0;
-int contador =0;
-cin<< 'ingrese un valor para calcular una suma'
-for [i = 1; i <= ivalor; i++]{
-  icontador+=i;
-}
-count >> 'el resultado es: '+%int,icontador;
-getch();
-exit 1;
+int main []{
+	int ivalor =0;
+	int contador =0;
+	cin<< 'ingrese un valor para calcular una suma'
+	for [i = 1; i <= ivalor; i++]{
+  	icontador+=i;
+	}
+	count >> 'el resultado es: '+%int,icontador;
+	getch();
+	returno 0;
 
 }
